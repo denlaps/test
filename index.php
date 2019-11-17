3d954229f9c3119628712838f2d67758396f134c
Denis Laptev <47497094+denlaps@users.noreply.github.com>
2019-11-17 05:17:30 +0300

    add mainSlider

--- index.php
+++ index.php
@@ -17,13 +17,30 @@ include ('./elements/header.php');
                 <!-- [MAIN CONTENT] -->
                 <main class="homePage">
                     <section class="firstSection">
-                        <figure class="mainSlider">
-                            <figcaption>
-                                <span class="mainSlider__caption"><?php echo $collection_row['season'] ?> <?php echo $collection_row['year'] ?></span>
-                                <h2><?php echo $collection_row['title'] ?></h2>
-                                <a href="/collection.php?id=<?= $collection_row['id'] ?>">Смотреть</a>
-                            </figcaption>
-                        </figure>
+                        <section class="mainSlider owl-carousel">
+                            <figure style="background: #9fa3af;">
+                                <img src="uploads/slider1.jpg" alt="">
+                                <figcaption>
+                                        <span class="mainSlider__caption"><?php echo $collection_row['season'] ?> <?php echo $collection_row['year'] ?></span>
+                                        <h2><?php echo $collection_row['title'] ?></h2>
+                                        <a href="/collection.php?id=<?= $collection_row['id'] ?>">Смотреть</a>
+                                </figcaption>
+                            </figure>
+                            <figure style="background: green;">
+                                <figcaption>
+                                    <span class="mainSlider__caption">Осень 2018</span>
+                                    <h2>Новая<br>коллекция</h2>
+                                    <a href="#">Смотреть</a>
+                                </figcaption>
+                            </figure>
+                            <figure style="background: brown;">
+                                <figcaption>
+                                    <span class="mainSlider__caption">Осень 2018</span>
+                                    <h2>Новая<br>коллекция</h2>
+                                    <a href="#">Смотреть</a>
+                                </figcaption>
+                            </figure>
+                        </section>
                         <figure class="autumnLook">
                             <img src="<?php echo $collection_row['image'] ?>" alt="">
                             <div class="autumnLook__wrapper">
@@ -87,6 +104,19 @@ include ('./elements/header.php');
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="libs/owlcarousel/owl.carousel.min.js"></script>
     <script src="js/main.js"></script>
+
+    <script>
+        /* == [OWL SLIDER] == */
+        $('.mainSlider').owlCarousel({
+            items: 1,
+            loop: true,
+            lazyLoad: true,
+            autoplay: true,
+            autoplayHoverPause: true,
+            nav: false,
+            dots: true
+        });
+    </script>
     <!-- [/SCRIPTS] -->
 </body>
 </html>
